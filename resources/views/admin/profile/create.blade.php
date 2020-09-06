79670074ea077cf3052ca8bf8f5e776657ccaae4
aoi <ruriirono-hanaba-6ttun-inti@docomo.ne.jp>
2020-09-06 06:52:22 +0000

    kadai_9/6

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charaset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name=="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>MyProfile</title>
+    </head>
+    <body>
+        <h1>自己紹介画面</h1>
+        <ul>名前</ul>
+        <ul>年齢</ul>
+        <ul>SNSのアドレス</ul>
+    </body>
+</html>
