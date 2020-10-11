9486f8e16b14b0834f3a45c899708cb003a56f18
aoi <ruriirono-hanaba-6ttun-inti@docomo.ne.jp>
2020-10-11 14:26:21 +0000

    PHP/Laravel 11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,16 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charaset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name=="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>MyProfile</title>
-    </head>
-    <body>
-        <h1>自己紹介画面</h1>
-        <ul>名前</ul>
-        <ul>年齢</ul>
-        <ul>SNSのアドレス</ul>
-    </body>
-</html>
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'プロフィール作成')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    //
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
-Route::get('XXX', 'Admin\AAA@bbb');
+Auth::routes();
 
-Route::get('admin/profile/create', 'Admin\ProfileController@add');
-
-Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+Route::get('/home', 'HomeController@index')->name('home');
