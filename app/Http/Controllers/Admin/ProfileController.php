e9e85136170e7e4a920937977192ce0274d6d3f3
aoi <ruriirono-hanaba-6ttun-inti@docomo.ne.jp>
2020-12-14 13:55:30 +0000

    profile編集が利用不可能

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\Controller;
 
 use App\Profile;
 
+use App\ProfileHistory;
+use Carbon\Carbon;
+
 class ProfileController extends Controller
 {
     //
@@ -30,16 +33,49 @@ class ProfileController extends Controller
       $profile->save();
       
         // admin/profile/createにリダイレクトする
-      return redirect('admin/profile/create');
+      return redirect('admin/profile');
     }
     
-    public function edit()
+    public function index(Request $request)
     {
-        return view('admin.profile.edit');
+        $cond_name = $request->cond_name;
+       if ($cond_name != '') {
+            $posts = Profile::where('name', $cond_name)->get();
+        } else {
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
     }
     
-    public function update()
+    public function edit(Request $request)
     {
-        return redirect('admin/profile/edit');
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
+    
+    public function update(Request $request)
+    {
+        $this->validate($request, Profile::$rules);
+        $profile = Profile::find($request->id);
+        $profile_form =$request->all();
+        unset($profile_form['_token']);
+        $profile->fill($profile_form)->save();
+        
+        $profile_history = new ProfileHistory;
+        $profile_history->profile_id = $profile->id;
+        $profile_history->edited_at =Carbon::now();
+        $profile_history->save();
+        
+        return redirect('admin/profile');
+    }
+    
+   public function delete(Request $request)
+ {
+     $profile = Profile::find($request->id);
+     $profile->delete();
+     return redirect('admin/profile/');
+ }
 }
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -28,9 +28,8 @@
                     </div>
                       <div class="form-group row">
                         <label class="col-md-2">性別</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="gender" value="{{ old('gender') }}">
-                        </div>
+                      <input type="radio" name="gender" value="男性">男性
+                      <input type="radio" name="gender" value="女性">女性
                     </div>
                       <div class="form-group row">
                         <label class="col-md-2">趣味</label>
